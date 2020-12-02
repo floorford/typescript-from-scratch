2c6d210316cabfa57ad39d8e909f9fc4c87ba354
Flo Orford <flo.orford@hotmail.com>
2020-12-02 13:01:28 +0000

    had some typos in my webpack config, tutorial i was follwing was using an older version of webpack so had to fix it work with the new webpack config and also a different command for the cli to start up the webpack server

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path"); //es5 import syntax - path built in with node
 const rules = [
   {
     test: /\.tsx?/, //regex for all files ending in .tsx
-    excludes: /node_modules/, // ignore these files
+    exclude: /node_modules/, // ignore these files
     loader: "babel-loader", // use babel loader to load the test files
   },
 ];
@@ -10,10 +10,10 @@ const rules = [
 // same as writing export/import in normal files, but node reads this and doesn't use ES6 onwards
 module.exports = {
   target: "web", // where we want to export our code to
-  node: "development", // compiles it according to use
+  mode: "development", // compiles it according to use
   entry: "./src/index.tsx", // where the entrypoint of our code is
   output: {
-    path: path.resolve(__dirname, "build"), // directory where it will create a new one, file name we want
+    path: path.resolve(__dirname, "dist"), // directory we want to create
     filename: "bundle.js", // file name it will compile the code to
   },
   module: { rules },
@@ -23,5 +23,6 @@ module.exports = {
   devServer: {
     contentBase: "./", // gets content from the root
     port: 5000, // where it exports the code to
+    open: true, // open browser when server running
   },
 };
